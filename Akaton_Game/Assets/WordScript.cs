60cd230dbdf35acccf278286c9238bd927ca4f9c
Maya Faerman <mayafa@mta.ac.il>
2023-11-17 03:18:55 +0200

    as

--- Akaton_Game/Assets/WordScript.cs
+++ Akaton_Game/Assets/WordScript.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
-using System.Collection;
-using System.Collection.Generic;
+//using System.Collection;
+//using System.Collection.Generic;
+using TMPro;
+using UnityEngine.UI;
 
 public class WordScript : MonoBehaviour
 {
-    public String wordToGuess;
+    public string wordToGuess;
     public TextMeshProUGUI[] wordButtonsTexts;
-    public gameObject[] wordButtons;
+    public GameObject[] wordButtons;
     public Color rightColor;
     public Color wrongColor;
 
@@ -20,25 +22,29 @@ public class WordScript : MonoBehaviour
     public void checkWord()
     {
         bool isOk = true;
-        for (int i = 0; i < wordToGuess.length; i++)
+        for (int i = 0; i < wordToGuess.Length; i++)
         {
-            if (!wordButtonsTexts[i].text.length) return;
-            if (wordButtonsTexts[i].text != wordToGuess[i]) isOk = false;
+            Debug.Log(wordButtonsTexts[i].text.Length +" ,"+ wordButtons[i].name+ " ,"+ wordButtonsTexts[i].text);
+            Debug.Log(wordButtons[i].name + " ," + wordToGuess[i]);
+            if (wordButtonsTexts[i].text.Length == 1) return;
+            if (!wordButtonsTexts[i].text.ToUpper().Equals(wordToGuess.ToUpper()[i])) isOk = false;
         }
         if (isOk)
         {
+            Debug.Log("OK");
             ColorButtons(rightColor, true);
         }
+        Debug.Log("BAD");
         ColorButtons(wrongColor, false);
     }
 
     public void ColorButtons(Color color,bool disableButton)
     {
-        for (int i = 0; i < wordToGuess.length; i++)
+        for (int i = 0; i < wordToGuess.Length; i++)
         {
-            if (wordButtons[i].getComponent<Image>.color == rightColor) continue;
-            wordButtons[i].getComponent<Image>.color = color;
-            wordButtons[i].setActive(disableButton);
+            if (wordButtons[i].GetComponent<Image>().color == rightColor) continue;
+            wordButtons[i].GetComponent<Image>().color = color;
+            wordButtons[i].GetComponent<TMP_InputField>().enabled = !disableButton;
         }
     }
 }
