26bf936b395069afe6ff9e3c10b27301e5dac8b7
leeyamoon <gabay32@gmail.com>
2023-11-30 18:22:42 +0200

    Fixed the properties of the canvas of the first game

--- Akaton_Game/Assets/scripts/GameManager.cs
+++ Akaton_Game/Assets/scripts/GameManager.cs
@@ -9,9 +9,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    private static GameManager self;
+    private int score;
+
+    private void Awake()
+    {
+        if (self == null)
+            self = this;
+    }
+
+    public static GameManager Shared()
+    {
+        return self;
+    }
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+        if (!PlayerPrefs.HasKey("Score"))
+        {
+            score = 0;
+            PlayerPrefs.SetInt("Score", 0);
+        }
     }
 
     public void RestartScene()
